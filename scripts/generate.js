9e9d0e81a5cca4c8802120bbc53ab8a5c6f63130
Manideep Polireddi <okmanideep@users.noreply.github.com>
2023-09-20 19:50:15 +0530

    Initial Slides

---
+++ scripts/generate.js
@@ -0,0 +1,71 @@
+import fs from 'fs'
+import path from 'path'
+import { Liquid } from 'liquidjs'
+import CleanCss from 'clean-css'
+import esbuild from 'esbuild'
+
+// es-build of src/index.js
+async function js() {
+	const result = await esbuild.build({
+		entryPoints: ["src/index.js"],
+		write: false,
+		bundle: true,
+	})
+	const [indexJs] = result.outputFiles
+	return indexJs.text
+}
+
+async function liveReloadJs({ debug = false }) {
+	if (!debug) return ''
+
+	const result = await esbuild.build({
+		entryPoints: ["src/live-reload.js"],
+		write: false,
+		bundle: true,
+	})
+	const [liveReloadJs] = result.outputFiles
+	return liveReloadJs.text
+}
+
+async function css() {
+	const directory = await fs.promises.readdir("src/css", {
+		withFileTypes: true
+	})
+	const cssFiles = directory
+		.filter(f => f.isFile())
+		.map(f => f.name)
+		.map(filename => path.join("src/css", filename))
+
+	const cssOutput = await (new CleanCss({
+		returnPromise: true
+	})).minify([
+		"node_modules/reveal.js/dist/reset.css",
+		"node_modules/reveal.js/dist/reveal.css",
+		...cssFiles
+	])
+
+	return cssOutput.styles
+}
+
+async function generate({ debug = false }) {
+	try {
+		await fs.promises.access("docs", fs.constants.R_OK | fs.constants.W_OK)
+	} catch (_) {
+		if (debug) console.log("Creating docs directory")
+		await fs.promises.mkdir("docs")
+	} finally {
+		// clean`/docs` folder if exists
+		await fs.promises.rm("docs/index.html", { force: true })
+	}
+
+	const liquid = new Liquid({
+		root: "src",
+		extname: ".html"
+	})
+
+	const [INLINE_JS, INLINE_CSS, INLINE_LIVE_RELOAD_JS] = await Promise.all([js(), css(), liveReloadJs({ debug })])
+	const htmlStream = await liquid.renderFileToNodeStream("index", { INLINE_CSS, INLINE_JS, INLINE_LIVE_RELOAD_JS })
+	await fs.promises.writeFile("docs/index.html", htmlStream)
+}
+
+export default generate
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Reveal from 'reveal.js'
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm'
+
+let deck = new Reveal({hash: true, plugins: [Highlight]})
+deck.initialize()
